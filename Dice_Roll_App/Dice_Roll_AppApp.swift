b4c2a82205ebfa5581330c3e83e6c2f258a7bb2e
ibrahimmedhat <doha.kroz0@gmail.com>
2023-09-20 14:53:22 +0300

    Initial Commit

---
+++ Dice_Roll_App/Dice_Roll_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_Roll_AppApp.swift
+//  Dice_Roll_App
+//
+//  Created by Ibrahim Medhat on 20/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Dice_Roll_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
